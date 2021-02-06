06d233a372bbc03c640b6c86a5aa9c60bb707e43
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-06 12:12:59 +0100

    added bar

--- data_vis.py
+++ data_vis.py
@@ -1,11 +1,31 @@
 
 from matplotlib import pyplot as plt
 
+###########
+#HISTOGRAM#
+###########
+
 #values on the y axis
 y = [14,3,6,9]
 
 #plotting histogram
 plt.hist(y)
+
+#showing plot
+plt.show()
+
+##########
+#BAR PLOT#
+##########
+
+#values on the x axis
+x = [5, 2, 9, 4, 7]
+
+#values on the y axis
+y = [14,3,6,9,1]
+
+#plotting bar
+plt.bar(x,y)
 
 #showing plot
 plt.show()
