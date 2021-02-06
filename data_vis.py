aa71821512d5da7e0d81ca493e998726bf39d140
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-06 12:19:46 +0100

    implemented pie chart example

--- data_vis.py
+++ data_vis.py
@@ -45,3 +45,27 @@ plt.plot(x,y)
 
 #showing plot
 plt.show()
+
+###########
+#PIE CHART#
+###########
+
+#The labels are the names for the parts
+label_set = '7', '6', '8+', '5-'
+
+#we give each label a size
+sizes = [20, 45, 15, 20]
+
+#we use explode to show a particular slide
+explode = (0, 0, 0, 0.1)
+
+#subplots
+fig1, ax1 =  plt.subplots()
+
+ax1.pie(sizes, explode=explode, labels=label_set, autopct='%1.1f%%', shadow=True, startangle=90)
+
+#make sure its a circle
+ax1.axis('equal')
+
+#show the plot
+plt.show()
